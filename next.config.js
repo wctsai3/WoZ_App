6e049ae43aa3bd413dc78bf31de6fd63299add64
wctsai3 <157747547+wctsai3@users.noreply.github.com>
2025-04-28 13:54:33 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // config options here
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
